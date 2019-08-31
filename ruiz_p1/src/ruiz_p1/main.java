7994053ffff0bacb18025b4c7075914963b0ec3e
Ricardo Ruiz <kxngvenom@gmail.com>
2019-08-30 23:01:54 -0400

    Adding Project Assignment 2 initial project files

---
+++ ruiz_p1/src/ruiz_p1/main.java
@@ -0,0 +1,10 @@
+package ruiz_p1;
+
+class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ruiz_p2/src/ruiz_p2/bmiCalc.java
@@ -0,0 +1,10 @@
+package ruiz_p2;
+
+public class bmiCalc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
