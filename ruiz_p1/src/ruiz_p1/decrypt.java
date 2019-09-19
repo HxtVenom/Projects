2ef1d5b2b5d5948e84bf562bb35e3ec4d51a12e4
Ricardo Ruiz <kxngvenom@gmail.com>
2019-09-18 20:19:22 -0400

    Project 2, Problem 1: encryption and decryption

--- ruiz_p1/src/ruiz_p1/decrypt.java
+++ ruiz_p1/src/ruiz_p1/decrypt.java
@@ -1,12 +1,32 @@
 package ruiz_p1;
+import java.util.Scanner;
 
 public class decrypt {
 
 	public static void main(String[] args) {
-		
+		//Variable Declerations
+		Scanner scnr = new Scanner(System.in);
+		int arrayLength = 4;
+		int[] inArray = new int[arrayLength];
 		int input;
 		
+		//Input
+		System.out.println("Please enter 4 digits: ");
+		input = scnr.nextInt();
 		
+		//Split input into array
+		inArray[3] = input % 10;
+		inArray[2] = (input / 10) % 10;
+		inArray[1] = (input / 100) % 10;
+		inArray[0] = (input / 1000) % 10;
+		
+		//Decryption
+		for(int i = 0; i < arrayLength; i++) {
+			inArray[i] = (inArray[i] + 10) - 7;
+		}
+		
+		//Output
+		System.out.println("Decrypted: " + inArray[2] + inArray[3] + inArray[0] + inArray[1]);
 		
 	}
 
--- ruiz_p1/src/ruiz_p1/encrypt.java
+++ ruiz_p1/src/ruiz_p1/encrypt.java
@@ -5,19 +5,20 @@ import java.util.Scanner;
 public class encrypt {
 
 	public static void main(String[] args) {
+		//Variable Declarations
 		Scanner scnr = new Scanner(System.in);
 		int input;
 		int one, two, three, four;
 		
+		//Input
 		System.out.println("Enter 4 digit integer: ");
 		input = scnr.nextInt();
 		
+		//Split input into individual digits
 		four = input % 10;
 		three = (input / 10) % 10;
 		two = (input / 100) % 10;
 		one = (input / 1000) % 10;
-		
-		System.out.println(input + " " + one + two + three + four); //Test Print
 		
 		//Encryption
 		one = (one + 7) % 10;
@@ -25,7 +26,8 @@ public class encrypt {
 		three = (three + 7) % 10;
 		four = (four + 7) % 10;
 		
-		
+		//Output
+		System.out.println("Encrypted: " + three + four + one + two);
 
 	}
 	
