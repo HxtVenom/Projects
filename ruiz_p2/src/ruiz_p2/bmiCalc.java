328245639e8e5a105e6016d49ca7b680898ad37b
Ricardo Ruiz <kxngvenom@gmail.com>
2019-09-18 20:48:40 -0400

    Project 2, Problem 2: BMI Calculator

---
+++ ruiz_p2/src/ruiz_p2/bmiCalc.java
@@ -0,0 +1,51 @@
+package ruiz_p2;
+import java.util.Scanner;
+
+public class bmiCalc {
+
+	public static void main(String[] args) {
+		//Variable Declarations
+		Scanner scnr = new Scanner(System.in);
+		double weight, height;
+		double bmi = 0;
+		String input;
+		int i = 0;
+		
+		//Input
+		System.out.println("----Welcome to the BMI calculator----");
+		System.out.println("Enter 'K' for kilos/meters or 'L' for pounds/inches: ");
+		input = scnr.next();
+		
+		while(i == 0) {
+			if(input.equalsIgnoreCase("K")) {
+				System.out.println("Enter height in meters: ");
+				height = scnr.nextDouble();
+				System.out.println("Enter weight in kilograms: ");
+				weight = scnr.nextDouble();
+				
+				bmi = (weight) / (height * height);
+				i++;
+			}else if(input.equalsIgnoreCase("L")) {
+				System.out.println("Enter height in inches: ");
+				height = scnr.nextDouble();
+				System.out.println("Enter weight in pounds: ");
+				weight = scnr.nextDouble();
+				
+				bmi = (703 * weight) / (height * height);
+				i++;
+			}else {
+				System.out.println("Invalid Input, try again");
+			}
+		}
+		
+		System.out.printf("Your BMI: %.2f\n\n", bmi);
+		System.out.println("-----------------BMI CHART----------------");
+		System.out.printf("BMI VALUE\t|\t Category\n");
+		System.out.printf("<18.5\t\t|\t Underweight\n");
+		System.out.printf("18.5-24.9\t|\t Normal Weight\n");
+		System.out.printf("25-29.9\t\t|\t Overweight\n");
+		System.out.printf(">30\t\t|\t Obesity\n");
+		
+	}
+
+}
